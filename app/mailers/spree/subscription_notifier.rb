bf0240edcaea57cda7c5d277957594a0d5c3aed5
beckyl <becky@naturalwellbeing.com>
2019-03-17 17:06:48 -0700

    add customs properties to hubspot emails

--- app/mailers/spree/subscription_notifier.rb
+++ app/mailers/spree/subscription_notifier.rb
@@ -3,9 +3,19 @@ class Spree::SubscriptionNotifier < Hubspot::TransactionEmail::Mailer
   def notify_confirmation(subscription)
     contact_properties = []
 
+    domain = 'http://' + subscription.parent_order.store.url + '/'
+    product_image = subscription.variant.images.length>0 ? subscription.variant.images.first.attachment.url(:original) : 'https://www.naturalwellbeing.com/assets/noimage/product.jpg'
+
     custom_properties = [
         { name: "number", value: subscription.number },
-        { name: "next_occurrence_at", value: subscription.next_occurrence_at.strftime("%B %d %Y at %I:%M %p") }
+        { name: "next_occurrence_at", value: subscription.next_occurrence_at.strftime("%B %d %Y at %I:%M %p") },
+        { name: "last_4_digit", value: subscription.source.last_digits },
+        { name: "product_name", value: subscription.variant.product.name },
+        { name: "sku", value: subscription.variant.sku },
+        { name: "quantity", value: subscription.quantity },
+        { name: "price", value: subscription.price },
+        { name: "product_url", value: domain + 'products/' + subscription.variant.product.slug},
+        { name: "image_url", value: product_image }
     ]
     notify_subscriber(subscription, "subscription_recieved_email_id", contact_properties, custom_properties)
   end
@@ -13,9 +23,19 @@ class Spree::SubscriptionNotifier < Hubspot::TransactionEmail::Mailer
   def notify_cancellation(subscription)
     contact_properties = []
 
+    domain = 'http://' + subscription.parent_order.store.url + '/'
+    product_image = subscription.variant.images.length>0 ? subscription.variant.images.first.attachment.url(:original) : 'https://www.naturalwellbeing.com/assets/noimage/product.jpg'
+
     custom_properties = [
         { name: "number", value: subscription.number },
-        { name: "cancellation_reasons", value: subscription.cancellation_reasons }
+        { name: "cancellation_reasons", value: subscription.cancellation_reasons },
+        { name: "last_4_digit", value: subscription.source.last_digits },
+        { name: "product_name", value: subscription.variant.product.name },
+        { name: "sku", value: subscription.variant.sku },
+        { name: "quantity", value: subscription.quantity },
+        { name: "price", value: subscription.price },
+        { name: "product_url", value: domain + 'products/' + subscription.variant.product.slug},
+        { name: "image_url", value: product_image }
     ]
     notify_subscriber(subscription, "subscription_canceled_email_id", contact_properties, custom_properties)
   end
@@ -23,9 +43,19 @@ class Spree::SubscriptionNotifier < Hubspot::TransactionEmail::Mailer
   def notify_for_next_delivery(subscription)
     contact_properties = []
 
+    domain = 'http://' + subscription.parent_order.store.url + '/'
+    product_image = subscription.variant.images.length>0 ? subscription.variant.images.first.attachment.url(:original) : 'https://www.naturalwellbeing.com/assets/noimage/product.jpg'
+
     custom_properties = [
         { name: "number", value: subscription.number },
-        { name: "next_occurrence_at", value: subscription.next_occurrence_at.strftime("%B %d %Y at %I:%M %p") }
+        { name: "next_occurrence_at", value: subscription.next_occurrence_at.strftime("%B %d %Y at %I:%M %p") },
+        { name: "last_4_digit", value: subscription.source.last_digits },
+        { name: "product_name", value: subscription.variant.product.name },
+        { name: "sku", value: subscription.variant.sku },
+        { name: "quantity", value: subscription.quantity },
+        { name: "price", value: subscription.price },
+        { name: "product_url", value: domain + 'products/' + subscription.variant.product.slug},
+        { name: "image_url", value: product_image }
     ]
     notify_subscriber(subscription, "subscription_order_reminder_email_id", contact_properties, custom_properties)
   end
@@ -33,18 +63,39 @@ class Spree::SubscriptionNotifier < Hubspot::TransactionEmail::Mailer
   def notify_for_cc_expiration(subscription)
     contact_properties = []
 
+    domain = 'http://' + subscription.parent_order.store.url + '/'
+    product_image = subscription.variant.images.length>0 ? subscription.variant.images.first.attachment.url(:original) : 'https://www.naturalwellbeing.com/assets/noimage/product.jpg'
+
     custom_properties = [
         { name: "number", value: subscription.number },
-        { name: "next_occurrence_at", value: subscription.next_occurrence_at.strftime("%B %d %Y at %I:%M %p") }
+        { name: "next_occurrence_at", value: subscription.next_occurrence_at.strftime("%B %d %Y at %I:%M %p") },
+        { name: "last_4_digit", value: subscription.source.last_digits },
+        { name: "product_name", value: subscription.variant.product.name },
+        { name: "sku", value: subscription.variant.sku },
+        { name: "quantity", value: subscription.quantity },
+        { name: "price", value: subscription.price },
+        { name: "product_url", value: domain + 'products/' + subscription.variant.product.slug},
+        { name: "image_url", value: product_image }
     ]
     notify_subscriber(subscription, "subscription_credit_card_expired_email_id", contact_properties, custom_properties)
   end
 
   def notify_for_oos(subscription)
     contact_properties = []
+
+    domain = 'http://' + subscription.parent_order.store.url + '/'
+    product_image = subscription.variant.images.length>0 ? subscription.variant.images.first.attachment.url(:original) : 'https://www.naturalwellbeing.com/assets/noimage/product.jpg'
+
     custom_properties = [
         { name: "number", value: subscription.number },
-        { name: "next_occurrence_at", value: subscription.next_occurrence_at.strftime("%B %d %Y at %I:%M %p") }
+        { name: "next_occurrence_at", value: subscription.next_occurrence_at.strftime("%B %d %Y at %I:%M %p") },
+        { name: "last_4_digit", value: subscription.source.last_digits },
+        { name: "product_name", value: subscription.variant.product.name },
+        { name: "sku", value: subscription.variant.sku },
+        { name: "quantity", value: subscription.quantity },
+        { name: "price", value: subscription.price },
+        { name: "product_url", value: domain + 'products/' + subscription.variant.product.slug},
+        { name: "image_url", value: product_image }
     ]
     notify_subscriber(subscription, "subscription_out_of_stock_email_id", contact_properties, custom_properties)
   end
@@ -52,9 +103,19 @@ class Spree::SubscriptionNotifier < Hubspot::TransactionEmail::Mailer
   def notify_for_placing_error(subscription)
     contact_properties = []
 
+    domain = 'http://' + subscription.parent_order.store.url + '/'
+    product_image = subscription.variant.images.length>0 ? subscription.variant.images.first.attachment.url(:original) : 'https://www.naturalwellbeing.com/assets/noimage/product.jpg'
+
     custom_properties = [
         { name: "number", value: subscription.number },
-        { name: "next_occurrence_at", value: subscription.next_occurrence_at.strftime("%B %d %Y at %I:%M %p") }
+        { name: "next_occurrence_at", value: subscription.next_occurrence_at.strftime("%B %d %Y at %I:%M %p") },
+        { name: "last_4_digit", value: subscription.source.last_digits },
+        { name: "product_name", value: subscription.variant.product.name },
+        { name: "sku", value: subscription.variant.sku },
+        { name: "quantity", value: subscription.quantity },
+        { name: "price", value: subscription.price },
+        { name: "product_url", value: domain + 'products/' + subscription.variant.product.slug},
+        { name: "image_url", value: product_image }
     ]
     notify_subscriber(subscription, "subscription_generic_issue_email_id", contact_properties, custom_properties)
   end
@@ -62,9 +123,19 @@ class Spree::SubscriptionNotifier < Hubspot::TransactionEmail::Mailer
   def notify_for_unpaused(subscription)
     contact_properties = []
 
+    domain = 'http://' + subscription.parent_order.store.url + '/'
+    product_image = subscription.variant.images.length>0 ? subscription.variant.images.first.attachment.url(:original) : 'https://www.naturalwellbeing.com/assets/noimage/product.jpg'
+
     custom_properties = [
         { name: "number", value: subscription.number },
-        { name: "next_occurrence_at", value: subscription.next_occurrence_at.strftime("%B %d %Y at %I:%M %p") }
+        { name: "next_occurrence_at", value: subscription.next_occurrence_at.strftime("%B %d %Y at %I:%M %p") },
+        { name: "last_4_digit", value: subscription.source.last_digits },
+        { name: "product_name", value: subscription.variant.product.name },
+        { name: "sku", value: subscription.variant.sku },
+        { name: "quantity", value: subscription.quantity },
+        { name: "price", value: subscription.price },
+        { name: "product_url", value: domain + 'products/' + subscription.variant.product.slug},
+        { name: "image_url", value: product_image }
     ]
     notify_subscriber(subscription, "subscription_reactivated_email_id", contact_properties, custom_properties)
   end
